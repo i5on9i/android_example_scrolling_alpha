4139bd3377256905028a8b381afae384f0dace05
i5on9i <gaedduck@gmail.com>
2016-03-25 11:20:11 +0900

    - add image view for toolbar and OnOffsetChangedListener for alphaed bg-imgae of the toolbar

--- app/src/main/java/com/namh/collapsedtoolbar/ScrollingActivity.java
+++ app/src/main/java/com/namh/collapsedtoolbar/ScrollingActivity.java
@@ -1,11 +1,17 @@
 package com.namh.collapsedtoolbar;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -24,5 +30,57 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        loadBackdrop();
+    }
+
+    private void loadBackdrop() {
+        final CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle("My New Title");
+
+        final int myDrawable = R.drawable.cheese_1;
+        final ImageView iv = (ImageView)findViewById(R.id.backdrop);
+        if (iv != null) {
+            iv.setImageResource(myDrawable);
+        }
+
+
+        AppBarLayout.OnOffsetChangedListener listener = new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if(collapsingToolbar.getHeight() + verticalOffset < 2 * ViewCompat.getMinimumHeight(collapsingToolbar)) {
+                    // collapsed
+                    iv.animate().alpha(0.3f).setDuration(600);
+                } else {
+                    // extended
+                    iv.animate().alpha(1f).setDuration(600);    // 1.0f means opaque
+                }
+            }
+        };
+
+        final AppBarLayout appbar = (AppBarLayout) findViewById(R.id.appbar);
+        appbar.addOnOffsetChangedListener(listener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
